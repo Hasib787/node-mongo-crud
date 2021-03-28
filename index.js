abc2954ce0e69f454158f96292c173d64dbd4839
Hasib787 <mr.hasib45@gmail.com>
2021-03-29 02:16:57 +0600

    added update product

--- index.js
+++ index.js
@@ -41,6 +41,16 @@ client.connect(err => {
         res.send('success')
       })
   })
+  
+  app.patch('/update/:id', (req, res) => {
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then(result => {
+      console.log(result);
+    })
+  })
 
   app.delete('/delete/:id', (req, res) => {
     productCollection.deleteOne({_id: ObjectId(req.params.id)})
